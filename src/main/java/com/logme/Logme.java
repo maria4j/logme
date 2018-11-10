d7b62325de8768dd618d482048e68e8b723c0c07
maria4j <manavorys@gmail.com>
2018-11-10 17:34:15 +0700

    Multiline messages with indentations (in progress): fixed method parameter names

--- src/main/java/com/logme/Logme.java
+++ src/main/java/com/logme/Logme.java
@@ -13,24 +13,24 @@ public final class Logme {
         return new MessageBuilderImpl(text);
     }
 
-    public static MessageBuilder newMessage(String text, int numberOfIndents) {
-        return new MessageBuilderImpl(text, numberOfIndents);
+    public static MessageBuilder newMessage(String text, int indent) {
+        return new MessageBuilderImpl(text, indent);
     }
 
     public static MessageBuilder newMessage(String id, String text) {
         return new MessageBuilderImpl(id, text);
     }
 
-    public static MessageBuilder newMessage(String id, String text, int numberOfIndents) {
-        return new MessageBuilderImpl(id, text, numberOfIndents);
+    public static MessageBuilder newMessage(String id, String text, int indent) {
+        return new MessageBuilderImpl(id, text, indent);
     }
 
     public static ParameterBuilder newParameters() {
         return new ParameterBuilderImpl();
     }
 
-    public static ParameterBuilder newParameters(int numberOfIndents) {
-        return new ParameterBuilderImpl(numberOfIndents);
+    public static ParameterBuilder newParameters(int indent) {
+        return new ParameterBuilderImpl(indent);
     }
 
 }
