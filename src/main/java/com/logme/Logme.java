3269bf177d628d7790986fb510f08e66714bdada
maria4j <manavorys@gmail.com>
2019-03-29 21:01:13 +0700

    Added javadoc to Logme and Message classes.

--- src/main/java/com/logme/Logme.java
+++ src/main/java/com/logme/Logme.java
@@ -1,14 +1,27 @@
 package com.logme;
 
+/**
+ * An entry point of Logme.
+ * <p>
+ * Start to create Logme messages by using {@code message}-methods.
+ */
 public final class Logme {
 
     private Logme() {
     }
 
+    /**
+     * Returns an empty message builder.
+     */
     public static Message message() {
         return new Message();
     }
 
+    /**
+     * Returns a message builder initialized to the contents of the specified string.
+     * 
+     * @param text the initial contents of the message.
+     */
     public static Message message(String text) {
         return new Message(text);
     }
